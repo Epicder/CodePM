36303b21a45d57baf8f0cde9a215a814f417d931
Epicder <epicder11@gmail.com>
2024-11-27 18:14:10 -0300

    First Approach, making the herarchy

---
+++ CodePM/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './css-components/Home.css'
+
+function Home() {
+  return (
+    <>
+      <h1>Hola</h1>
+    </>
+  )
+}
+
+export default Home
---
+++ CodePM/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Home from './components/Home.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <Home />
+  </>,
+)
